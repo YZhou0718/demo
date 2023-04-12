e8f466ac5be436558b36ce530e4a4c896a3ac852
YZhou0718 <zy0718@seas.upenn.edu>
2023-04-12 01:14:46 -0400

    add function1

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test for cit594!");
     }
+
+    public void Function1() {
+        System.out.println("This is function 1");
+    }
 }
